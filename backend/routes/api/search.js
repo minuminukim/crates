399ce957586dd20689723c952a936094c579b9d0
Minu Kim <88391760+minuminukim@users.noreply.github.com>
2022-02-15 18:17:18 -0500

    Build searchArtistByName helper

--- backend/routes/api/search.js
+++ backend/routes/api/search.js
@@ -1,46 +1,35 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+const { searchArtistByName } = require('../../utils/spotify-api');
 const axios = require('axios');
 const qs = require('qs');
-const { setAccessTokenCookie } = require('../../utils/spotify-api');
-const { spotifyConfig } = require('../../config');
 
 const router = express.Router();
 
-const CLIENT_ID = spotifyConfig.clientID;
-const CLIENT_SECRET = spotifyConfig.clientSecret;
-const ENCODED_PAYLOAD = new Buffer(`${CLIENT_ID}:${CLIENT_SECRET}`).toString(
-  'base64'
-);
+// router.get(
+//   '/',
+//   asyncHandler(async (req, res) => {
+//     res.json({ message: 'howdy' });
+//     getAccessToken();
+//   })
+// );
 
-const getAccessToken = async () => {
-  const tokenURL = 'https://accounts.spotify.com/api/token';
-  const data = qs.stringify({ grant_type: 'client_credentials' });
-  const headers = {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      Authorization: `Basic ${ENCODED_PAYLOAD}`,
-    },
-  };
-
-  try {
-    const response = await axios.post(tokenURL, data, headers);
-    const token = response.data;
-    const { access_token } = response.data;
-    console.log('success');
-    console.log('data', response.data);
-    console.log(access_token);
-    return access_token;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
+// search artist
 router.get(
   '/',
-  asyncHandler(async (req, res) => {
-    res.json({ message: 'howdy' });
-    getAccessToken();
+  asyncHandler(async (req, res, next) => {
+    const { spotifyToken } = req.cookies;
+    const { artistName } = req.body;
+    const data = await searchArtistByName(spotifyToken, 'kanye west');
+
+    if (data && data.errors) {
+      res.json({ data });
+    } else {
+      console.log('data', data);
+      const artists = data.artists
+      // console.log('artists.items', data.artists.items);
+      res.json({ artists });
+    }
   })
 );
 
--- backend/utils/spotify-api.js
+++ backend/utils/spotify-api.js
@@ -4,7 +4,7 @@ const { spotifyConfig } = require('../config');
 
 const CLIENT_ID = spotifyConfig.clientID;
 const CLIENT_SECRET = spotifyConfig.clientSecret;
-const ENCODED_PAYLOAD = new Buffer(`${CLIENT_ID}:${CLIENT_SECRET}`).toString(
+const ENCODED_PAYLOAD = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString(
   'base64'
 );
 
@@ -36,8 +36,8 @@ const getAccessToken = async (next) => {
   }
 };
 
-const setAccessTokenCookie = async (res) => {
-  const { access_token, expires_in } = await getAccessToken();
+const setAccessTokenCookie = async (res, next) => {
+  const { access_token, expires_in } = await getAccessToken(next);
   const isProduction = process.env.NODE_ENV === 'production';
 
   res.cookie('spotifyToken', access_token, {
@@ -50,4 +50,39 @@ const setAccessTokenCookie = async (res) => {
   return token;
 };
 
-module.exports = { getAccessToken, setAccessTokenCookie };
+class SearchError extends Error {
+  constructor(message, status, title, errors) {
+    super(message);
+    this.name = 'SearchError';
+    this.status = status;
+    this.title = title;
+    this.errors = errors;
+  }
+}
+
+// TODO: error handling for: 1) expired token(401), 2) rate limit (403)
+
+// SEARCH HELPERS
+const searchArtistByName = async (accessToken, artistName) => {
+  // const apiURL = `https://api.spotify.com/v1/search?q=name:${artistName}&type=artist&include_external=audio`;
+  const url = `https://api.spotify.com/v1/search?q=${artistName}&type=artist`;
+
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log('error@@@@@@@@@@', JSON.stringify(error));
+    return new SearchError(
+      `No results found for ${artistName}.`,
+      400,
+      'Search Error',
+      { search: `${this.message}` }
+    );
+  }
+};
+
+module.exports = { getAccessToken, setAccessTokenCookie, searchArtistByName };
