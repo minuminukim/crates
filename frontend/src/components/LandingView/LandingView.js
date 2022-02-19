b108adb8ec2db08224bb762ea60bc7aa2aeb33ed
Minu Kim <88391760+minuminukim@users.noreply.github.com>
2022-02-19 03:50:38 -0500

    Landing page formatting

--- frontend/src/components/LandingView/LandingView.js
+++ frontend/src/components/LandingView/LandingView.js
@@ -10,31 +10,36 @@ const LandingView = () => {
   const handleDemoUser = () =>
     dispatch(loginDemo()).then(() => history.push('/home'));
   return (
-    <>
-      <div className="page-container landing-page">
-        <div className="backdrop-container">
-          <div className="backdrop"></div>
-        </div>
-        <div className="credit-container">
-          <span className="credit-link">
-            DJ Rashad, DJ Spinn, Traxman (2013), Photo by{' '}
-          </span>
-          <a
-            className="credit-link"
-            href="https://www.erezavissar.com/"
-            target="_blank"
-          >
-            Erez Avissar.
-          </a>
-        </div>
-        <Button
-          className="btn-demo-user"
-          label="DEMO"
-          size="medium"
-          onClick={handleDemoUser}
-        />
+    <div className="page-container landing-page">
+      {/* <div className="backdrop-container"> */}
+        <div className="backdrop"></div>
+      {/* </div> */}
+      <div className="credit-container">
+        <span className="credit-link">
+          DJ Rashad, DJ Spinn, Traxman (2013), Photo by{' '}
+        </span>
+        <a
+          className="credit-link"
+          href="https://www.erezavissar.com/"
+          target="_blank"
+        >
+          Erez Avissar.
+        </a>
       </div>
-    </>
+      <div className="hero">
+        <h1 className="hero-heading">
+          Track albums you've listened to.
+          <br /> Save those you want to hear.
+          <br /> Tell your friends what's good.
+        </h1>
+      </div>
+      <Button
+        className="btn-demo-user"
+        label="GET STARTED –– DEMO"
+        // size="medium"
+        onClick={handleDemoUser}
+      />
+    </div>
   );
 };
 
