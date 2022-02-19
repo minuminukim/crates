abb651d66b8f19f2dd147d4fa28821dbe83e64f8
Minu Kim <88391760+minuminukim@users.noreply.github.com>
2022-02-19 03:25:51 -0500

    Apply some styles to landing page

--- frontend/src/components/LandingView/LandingView.js
+++ frontend/src/components/LandingView/LandingView.js
@@ -2,7 +2,6 @@ import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { loginDemo } from '../../store/session';
 import Button from '../Button/Button';
-
 import './LandingView.css';
 
 const LandingView = () => {
@@ -11,14 +10,31 @@ const LandingView = () => {
   const handleDemoUser = () =>
     dispatch(loginDemo()).then(() => history.push('/home'));
   return (
-    <div>
-      <Button
-        className="btn-demo-user"
-        label="DEMO"
-        size="medium"
-        onClick={handleDemoUser}
-      />
-    </div>
+    <>
+      <div className="page-container landing-page">
+        <div className="backdrop-container">
+          <div className="backdrop"></div>
+        </div>
+        <div className="credit-container">
+          <span className="credit-link">
+            DJ Rashad, DJ Spinn, Traxman (2013), Photo by{' '}
+          </span>
+          <a
+            className="credit-link"
+            href="https://www.erezavissar.com/"
+            target="_blank"
+          >
+            Erez Avissar.
+          </a>
+        </div>
+        <Button
+          className="btn-demo-user"
+          label="DEMO"
+          size="medium"
+          onClick={handleDemoUser}
+        />
+      </div>
+    </>
   );
 };
 
