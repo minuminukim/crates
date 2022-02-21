9d1ed8a54d7c62fd1bb9053214d0a844229357ec
Minu Kim <88391760+minuminukim@users.noreply.github.com>
2022-02-20 20:20:09 -0500

    Commit the merge conflcit

---
+++ frontend/src/views/Landing/Landing.js
@@ -0,0 +1,46 @@
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { loginDemo } from '../../store/session';
+import Button from '../../components/Button';
+import './Landing.css';
+
+const Landing = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const handleDemoUser = () =>
+    dispatch(loginDemo()).then(() => history.push('/'));
+  return (
+    <div className="page-container landing-page">
+      {/* <div className="backdrop-container"> */}
+      <div className="backdrop"></div>
+      {/* </div> */}
+      <div className="credit-container">
+        <span className="credit-link">
+          DJ Rashad, DJ Spinn, Traxman (2013), Photo by{' '}
+        </span>
+        <a
+          className="credit-link"
+          href="https://www.erezavissar.com/"
+          target="_blank"
+        >
+          Erez Avissar.
+        </a>
+      </div>
+      <div className="hero">
+        <h1 className="hero-heading">
+          Track albums you've listened to.
+          <br /> Save those you want to hear.
+          <br /> Tell your friends what's good.
+        </h1>
+      </div>
+      <Button
+        className="btn-demo-user"
+        label="GET STARTED –– DEMO"
+        // size="medium"
+        onClick={handleDemoUser}
+      />
+    </div>
+  );
+};
+
+export default Landing;
