ec150d47fb6946d3378f34d3e348acbf97e8b985
Minu Kim <88391760+minuminukim@users.noreply.github.com>
2022-02-21 15:29:27 -0500

    Basic formatting on FeedPost

--- frontend/src/components/FeedPost/FeedPost.js
+++ frontend/src/components/FeedPost/FeedPost.js
@@ -1,17 +1,22 @@
 import { ListSpread } from '../ListCard';
 import { FaUserCircle } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import './FeedPost.css';
 
 const FeedPost = ({ list }) => {
   return (
     <li className="feed-post">
-      <div><ListSpread albums={list.albums} listID={list.id} /></div>
       <div>
-        <h3>{list.title}</h3>
+        <ListSpread albums={list.albums} listID={list.id} />
+      </div>
+      <div className="feed-post-right">
+        <h3>
+          <Link to={`/lists/${list.id}`}>{list.title}</Link>
+        </h3>
         <div>
           <FaUserCircle />
           <span>{list.User.username}</span>
-          <span>{list.albums.length}</span>
+          <span>{`${list.albums.length} albums`}</span>
         </div>
         <div>
           <p className="description">{list.description}</p>
