f4ef24e8021c22186930f455e41b1a0a71070f79
Minu Kim <88391760+minuminukim@users.noreply.github.com>
2022-02-23 23:36:07 -0500

    Add dropdown link to redirect to new list page from nav

--- frontend/src/components/Navigation/LogButton.js
+++ frontend/src/components/Navigation/LogButton.js
@@ -1,16 +1,44 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { AiOutlineDown, AiOutlinePlus } from 'react-icons/ai';
 import './LogButton.css';
 
 const LogButton = ({ handleLogClick, handleToggleClick }) => {
+  const [showButton, setShowButton] = useState(false);
+
+  const openButton = () => {
+    if (showButton) return;
+    setShowButton(true);
+  };
+
+  useEffect(() => {
+    if (!showButton) return;
+
+    const closeButton = () => {
+      setShowButton(false);
+    };
+
+    document.addEventListener('click', closeButton);
+
+    return () => document.removeEventListener('click', closeButton);
+  }, [showButton]);
+
   return (
     <button className="log-button">
       <div className="log-button-left" onClick={handleLogClick}>
         <AiOutlinePlus />
         <p>LOG</p>
       </div>
-      <span className="log-button-toggle" onClick={handleToggleClick}>
+      <span className="log-button-toggle" onClick={openButton}>
         <AiOutlineDown />
       </span>
+      {showButton && (
+        <div className="btn-link">
+          <Link exact to="/lists/new">
+            Start a new list...
+          </Link>
+        </div>
+      )}
     </button>
   );
 };
