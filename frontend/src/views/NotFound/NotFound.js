ceaed3aa1ab26e99ea124e502f323d55b4acf363
Minu Kim <88391760+minuminukim@users.noreply.github.com>
2022-02-28 00:03:40 -0500

    Add image to error page

--- frontend/src/views/NotFound/NotFound.js
+++ frontend/src/views/NotFound/NotFound.js
@@ -1,15 +1,19 @@
+import error from '../../images/garage.jpeg';
 import { Link } from 'react-router-dom';
+import './NotFound.css';
 
 const NotFound = () => {
   return (
     <div className="page-container not-found">
-      <h1>
-        <Link to="/">Crates</Link>
-      </h1>
       <h2 className="message">
         Sorry, we can't find the page you've requested.
       </h2>
       <p className="message">Perhaps you imagined it?</p>
+      <img
+        src={error}
+        alt="An evening at Paradise Garage."
+        className="error-backdrop"
+      />
     </div>
   );
 };
